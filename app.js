735da594a797a81c0051cbcd443d7f4322b64820
S.M.ABTAHI NOOR <73340940+19smabtahinoor@users.noreply.github.com>
2021-03-17 01:55:31 +0600

    Version 1.3

--- app.js
+++ app.js
@@ -35,3 +35,104 @@ function hideSideBar(){
     sideBar.style.transition="0.8s";
     sideBar.style.opacity="0";
 }
+
+
+
+
+//counting numbers
+const countingNumber1 = document.querySelector('.countingNumber1')
+const countingNumber2 = document.querySelector('.countingNumber2')
+const countingNumber3 = document.querySelector('.countingNumber3')
+const countingNumber4 = document.querySelector('.countingNumber4')
+
+//1st number count
+let count1 = 0
+let count1Interval = setInterval( () =>{
+    count1++
+    if(count1 >= 1200){
+        clearInterval(count1Interval)
+    }
+    countingNumber1.innerHTML = count1
+},0.001)
+
+
+//2nd number count
+let count2 = 0
+let count2Interval = setInterval( () =>{
+    count2++
+    if(count2 >= 2080){
+        clearInterval(count2Interval)
+    }
+    countingNumber2.innerHTML = count2
+},0.001)
+
+//3rd number count
+let count3 = 0
+let count3Interval = setInterval( () =>{
+    count3++
+    if(count3 >= 4000){
+        clearInterval(count3Interval)
+    }
+    countingNumber3.innerHTML = count3
+},0.001)
+
+
+//4th number count
+let count4 = 0
+let count4Interval = setInterval( () =>{
+    count4++
+    if(count4 >= 4000){
+        clearInterval(count4Interval)
+    }
+    countingNumber4.innerHTML = count4
+},0.001)
+
+
+
+
+
+//image popup image
+const projectImage = document.querySelector('.projectImage')
+const projectImageBackdrop = document.querySelector('.projectImageBackdrop')
+const closetextProjectImage = document.querySelector('.closeText')
+const ProjectImagePopup = document.querySelector('.projectImagePopup')
+
+
+
+projectImage.addEventListener('click',() => {
+    ProjectImagePopup.classList.add('projectImagePopupActive')
+    projectImageBackdrop.classList.add('projectImageBackdropActive')
+})
+
+
+closetextProjectImage.addEventListener('click',() => {
+    ProjectImagePopup.classList.remove('projectImagePopupActive')
+    projectImageBackdrop.classList.remove('projectImageBackdropActive')
+})
+
+
+
+//imagepopup another
+const serviceImage = document.querySelectorAll('.serviceimage')
+const projectImagePopup1 = document.querySelector('.projectImagePopup1')
+const projectImageBackdrop1 = document.querySelector('.projectImageBackdrop1')
+const closetextServiceImage = document.querySelector('.closeText1')
+const eachImage = document.querySelector('.eachImage')
+
+serviceImage.forEach( (images) => {
+    images.addEventListener('click',(ev) => {
+        projectImagePopup1.classList.add('projectImagePopup1Active')
+        projectImageBackdrop1.classList.add('projectImageBackdrop1Active')
+        eachImage.setAttribute("src",ev.target.src)
+
+    })
+})
+
+serviceImage.forEach( (images) => {
+    closetextServiceImage.addEventListener('click',(ev) => {
+        projectImagePopup1.classList.remove('projectImagePopup1Active')
+        projectImageBackdrop1.classList.remove('projectImageBackdrop1Active')
+        
+
+    })
+})
